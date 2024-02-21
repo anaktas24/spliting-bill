5654b737c305042bfe8e99db35c377b3f0ec2e53
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-21 22:37:40 +0100

    created the functions

--- splitbill/src/App.jsx
+++ splitbill/src/App.jsx
@@ -5,9 +5,64 @@ function App() {
 
 
   return (
-    <>
+    <div className="App">
+      <TipCalculator/>
+    </div>
+  )
+}
 
-    </>
+function TipCalculator(){
+  return(
+    <div>
+      <BillInput/>
+      <SelectPercentage>How did you like the service?</SelectPercentage>
+      <SelectPercentage>How did your friend like the service?</SelectPercentage>
+      <Output/>
+      <Reset/>
+    </div>
+  )
+}
+
+function BillInput(){
+  return(
+    <div>
+      <label>How much was the bill?</label>
+      <input type='text' placeholder='Bill value'/>
+    </div>
+  )
+}
+
+function SelectPercentage({children}){
+  return(
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">Acceptable (5%)</option>
+        <option value="10">Good (10%)</option>
+        <option value="20">Excelent (20%)</option>
+      </select>
+    </div>
+  )
+}
+
+function Output(){
+  return(
+    <div>
+      <h3>
+        You pay X ($A -$B tip)
+      </h3>
+    </div>
+  )
+}
+
+function Reset(bgColor, textColor){
+  return(
+    <div>
+      <button >
+
+      </button>
+    </div>
   )
 }
 
